c887ee3cfebefed42701256ef7b61fc7d0eaecea
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-02-22 14:25:50 -0800

    Delete Team_project/javascript/imscript.js

--- Team_project/javascript/imscript.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('myButton').addEventListener('click', function() {
-            document.getElementById('message').textContent = 'Thank you for your submission!';
-        });
