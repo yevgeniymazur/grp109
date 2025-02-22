286dd9c3dc00e19907484e4702360013b510deeb
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-02-22 13:16:01 -0800

    clickButton.js

---
+++ Team_project/javascript/clickButton.js
@@ -0,0 +1,3 @@
+document.getElementById('myButton').addEventListener('click', function() {
+            document.getElementById('message').textContent = 'Thank you for your submission!';
+        });
