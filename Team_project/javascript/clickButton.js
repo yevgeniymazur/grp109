eacc17ea3b788e61aefb4e6d14b4fb629a90f5d5
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-02-23 19:58:58 -0800

    Delete Team_project directory

--- Team_project/javascript/clickButton.js
+++
@@ -1,4 +0,0 @@
-//clickButton Isaac M
-document.getElementById('myButton').addEventListener('click', function() {
-            document.getElementById('message').textContent = 'Thank you for your submission!';
-        });
